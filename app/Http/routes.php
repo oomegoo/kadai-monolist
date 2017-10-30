0f4a49456c4dbfd2b96ad1e2ee6d5863e01a2079
meg <oobridgeoo@gmail.com>
2017-10-30 06:22:48 +0000

    rakuten api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,7 @@ Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+//Middleware設定
+Route::group(['middleware' => 'auth'], function(){
+    Route::resource('items','ItemsController',['only' => ['create']]);
+});
